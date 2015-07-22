8e4b31391fb1b38dcceebc5d36518ff18493c7ce
Nick Saunders <nick@saunde.rs>
2015-07-22 16:24:51 -0700

    Cabal fixes

--- library/Data/Telephony/Meid.hs
+++ library/Data/Telephony/Meid.hs
@@ -1,11 +1,32 @@
+{-|
+
+Module      : Data.Telephony.Meid
+Description : Contains a data structure that represents a Mobile Equipment Identifier and
+              various MEID functions.
+Copyright   : (c) 2015 Nick Saunders
+License     : MIT
+Maintainer  : nick@saunde.rs
+
+-}
 module Data.Telephony.Meid (Meid (HexMeid, DecMeid), Data.Telephony.Meid.showHex, showDec) where
 
 import Data.Char (toUpper)
 import Numeric (readHex, showHex)
 
-data Meid = HexMeid [Char] | DecMeid [Char]
+-- | The 'Meid' type represents a Mobile Equipment Identifier (MEID).
+data Meid
+    = HexMeid [Char] -- ^ Constructs a MEID given a MEID string in hexadecimal format.
+    | DecMeid [Char] -- ^ Constructs a MEID given a MEID string in decimal format.
+
+-- | The 'showHex' function shows a MEID in hexadecimal format.
+showHex :: Meid -- ^ The MEID to show in hexadecimal format
+        -> [Char] -- ^ The MEID in hexadecimal format
 showHex (HexMeid m) = m
 showHex (DecMeid m) = uppercase $ (hex (firstPart $ DecMeid m)) ++ (hex (secondPart $ DecMeid m))
+
+-- | The 'showDec' function shows a MEID in decimal format.
+showDec :: Meid -- ^ The MEID to show in decimal format
+        -> [Char] -- ^ The MEID in decimal format
 showDec (DecMeid m) = m
 showDec (HexMeid m) = (++)
     (unwrap $ readHex (firstPart $ HexMeid m))
